fa4e9154f2317322083af12b97f4003607012ae2
MuthuBalaji98 <muthubalaji1998@gmail.com>
2022-09-11 20:40:29 +0530

    concat

---
+++ main.js
@@ -0,0 +1,10 @@
+let subject1=["Tamil","English"];
+let subject2=["Maths","Science","Social"];
+console.log("original array1 :",subject1);
+console.log("original array2 :",subject2);
+console.log("length of the array1 is "+subject1.length);
+console.log("length of the array2 is "+subject2.length);
+console.log("method : array1.concat(array2)");
+let result=subject1.concat(subject2);
+console.log("change in array : ",result);
+console.log("return: length of array after concat() is "+result.length);
